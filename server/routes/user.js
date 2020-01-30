218c1ff90aa61415ca85bfdf5131706542703aa0
praneetha-tabbibbu <tabbibbu@gmail.com>
2020-01-30 12:09:57 +0530

    added new data

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/user', (req, res) => {
+    User.find();
+    res.send(User);
+});
+
+router.post('/user', (req, res) => {
     res.send('user');
 });
 
